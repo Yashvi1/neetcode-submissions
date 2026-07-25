66e2613fa7b6609f6bf4c66fb3ae2ad4c7192969
Yashvi Uttam <42138176+Yashvi1@users.noreply.github.com>
2026-07-25 12:54:51 +0530

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {
+            "{" : "}",
+            "(" : ")",
+            "[" : "]"
+        }
+        res = False
+        open_bracket = "({["
+        close_brackets = ")}]"
+        order = []
+        for ch in s:
+            if ch in open_bracket:
+                print("in open brackt")
+                order.append(ch)
+            elif len(order) > 0 and (brackets[order.pop()] == ch):
+                print("popping")
+            else:
+                return False
+        return len(order) == 0 
