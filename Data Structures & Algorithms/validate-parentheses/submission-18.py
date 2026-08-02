33e14f67cbe91c2a70544e1f8c895c28a62f6a0d
Yashvi Uttam <42138176+Yashvi1@users.noreply.github.com>
2026-08-02 10:58:57 +0530

    Add: validate-parentheses - submission-18

---
+++ Data Structures & Algorithms/validate-parentheses/submission-18.py
@@ -0,0 +1,29 @@
+class Solution:
+    
+    def isValid(self, s: str) -> bool:
+        pair = {
+        "{":"}",
+        "(":")",
+        "[":"]"
+        }
+
+        open = ["(", "[", "{"]
+
+        bracket = []
+        for b in s:
+            if b in open:
+                bracket.append(b)
+            elif b not in open:
+                if not bracket:
+                    return False
+                if pair[bracket[-1]] != b:
+                    return False
+                else:
+                    bracket.pop()
+        if bracket:
+            return False
+        return True
+            
+
+
+        
